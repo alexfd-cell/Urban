170abe0da89f8572a014039804341f69f56c91f3
alexfd-cell <shestakov_oleg@mail.ru>
2024-08-03 15:44:11 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,67 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return (f'Название: {self.name}, количество этажей: {self.number_of_floors}')
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, other):
+        if isinstance(other, House):
+            return House(self.name, self.number_of_floors + other.self.name)
+        else:
+            return House(self.name, self.number_of_floors + other)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('Акация', 20)
+
+
+print(h1)
+print(h2)
+print(h1==h2)
+h1 = h1 + 10
+print(h1)
+print(h1==h2)
+h1 += 10
+print(h1)
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
