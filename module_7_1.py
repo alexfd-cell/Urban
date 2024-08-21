e6efe743e00c9b37b535c8768a956dda5f7bab15
alexfd-cell <shestakov_oleg@mail.ru>
2024-08-22 00:25:30 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,4 +1,3 @@
-
 class Product:
 
     def __init__(self, name, weight, category):
@@ -12,21 +11,38 @@ class Product:
 class Shop:
     __file_name = 'products.txt'
 
-    def get_products():
+    def get_products(self):
         file = open(Shop.__file_name, 'r')
         pProd = file.read()
         file.close()
         return pProd
 
-    def add(*products):
-        Shop.products = products
-        print(products)
+    def add(self, *products):
+        self.products = products
+
+        GetProduct = self.get_products()
+        c, lst, addProd = 0, [], []
+        if GetProduct != '\n':            addProd.append(GetProduct)
+        lst.append(GetProduct[c:GetProduct.find(',', c)])
+        for i in range(GetProduct.count('\n')):
+            c = GetProduct.find('\n', c + 1)
+            lst.append(GetProduct[c + 1:GetProduct.find(',', c)])
+
+        for item in self.products:
+            if lst.count(item.name):
+                print(f'Продукт {item.name} уже есть в магазине')
+            else:
+                addProd.append(item.name + ', ' + str(item.weight) + ', ' + item.category)
+
+        with open(Shop.__file_name, 'w') as file:
+            for i in addProd:
+                file.write(i + '\n')
 
 
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
-p3 = Product('Potato', 5.5, 'Vegetables')
-# print(p2) # __str__
-print(Shop.get_products())
-Shop.add()
+p3 = Product('Orange', 5.5, 'Fruits')
+print(p2) # __str__
+s1.add(p1, p2, p3)
+print(s1.get_products())
