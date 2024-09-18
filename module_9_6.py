ca85000c5b4fcb088d112f5a6b94d25649b479df
alexfd-cell <shestakov_oleg@mail.ru>
2024-09-18 19:59:17 +0300

    Update module_9_6.py

--- module_9_6.py
+++ module_9_6.py
@@ -1,23 +1,9 @@
-from itertools import combinations
-
-# Variant 1
 def  all_variants(text):
     for i in range(len(text)):
-        if i == 0:
-            for j in range(1, len(text)+1):
-                yield text[j-1:j]
-        if i == 1:
-            for j in range(len(text)-1):
-                yield text[j:j+i+1]
-        if i == 2:
-            yield text[0:i + 1]
-
-# Variant 2
-# def  all_variants(text):
-#     for i in range(1,len(text)+1):
-#         yield list(combinations(text, i))
+        for j in range(i, len(text)):
+            yield text[i:j+1]
 
 
 a = all_variants("abc")
 for i in a:
-    print(*i)
+    print(i)
