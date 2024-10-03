02c3701ae019f5e5b12fbcff08bd17404feaf6e5
alexfd-cell <shestakov_oleg@mail.ru>
2024-10-03 23:32:32 +0300

    Add files via upload

---
+++ tests_12_1.py
@@ -0,0 +1,43 @@
+import unittest
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+        return self.distance
+
+    def walk(self):
+        self.distance += 5
+        return self.distance
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        obj1 = Runner('Horse')
+        for i in range(10):
+            obj1.walk()
+        self.assertEqual(obj1.distance, 50)
+
+    def test_run(self):
+        obj2 = Runner('Horse')
+        for i in range(10):
+            obj2.run()
+        self.assertEqual(obj2.distance, 100)
+
+    def test_challenge(self):
+        obj3 = Runner('Eagle')
+        obj4 = Runner('Kite')
+        for i in range(10):
+            obj3.run()
+            obj4.walk()
+        self.assertNotEqual(obj3.distance,obj4.distance)
+
+if __name__ == '__main__':
+    unittest.main()
